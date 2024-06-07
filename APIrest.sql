388c662c2b5c945f623f5210f77ffce65d34adf2
AdrianVR1 <164523441+AdrianVR1@users.noreply.github.com>
2024-06-06 20:38:47 -0500

    Add files via upload

---
+++ APIrest.sql
@@ -0,0 +1,8 @@
+create database db_client_backend;
+use db_client_backend;
+
+select * from clients;
+select * from cities;
+select * from products;
+select * from invoices;
+select * from item_invoices;
